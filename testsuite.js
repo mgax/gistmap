f624458d29422b9d96025b606629e137e446da69
Alex Morega <alex@grep.ro>
2014-03-07 00:38:54 +0200

    basic test for choropleth layer

--- testsuite.js
+++ testsuite.js
@@ -2,6 +2,9 @@
 'use strict';
 
 
+if(! window.Tabletop) { window.Tabletop = {init: function() {}}; }
+
+
 var createLeafletMap = function() {
   return L.map($('<div>')[0]);
 };
@@ -54,6 +57,53 @@ describe("layerRender.background", function() {
     var layer = getFirstLayer(map);
     expect(layer._url).toContain('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     expect(layer.options.attribution).toContain('http://osm.org/copyright');
+  });
+});
+
+
+describe("layerRender.choropleth", function() {
+  beforeEach(function() {
+    jasmine.Ajax.install();
+    spyOn(Tabletop, 'init');
+  });
+
+  afterEach(function() {
+    jasmine.Ajax.uninstall();
+  })
+
+  it("creates a layer with blank data", function() {
+    var dataUrl = 'https://docs.google.com/spreadsheet/ccc?key=' +
+        '0AlBmcLkxpBOXdEEzSE1HWTdWeGYxTG9ja0l4c2VyRHc&usp=drive_web#gid=0';
+    var featureUrl = 'url-for-the-feature';
+    var geojson = JSON.stringify({
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {id: 1},
+          geometry: {
+            type: 'Polygon',
+            coordinates: [[[0, 0], [1, 0], [1, 1], [0, 0]]]
+          }
+        }
+      ]
+    });
+
+    var map = createLeafletMap();
+    M.layerRender.choropleth(map, {data: dataUrl, features: featureUrl});
+
+    var initOptions = Tabletop.init.calls.first().args[0];
+    expect(initOptions.key).toEqual(dataUrl);
+    initOptions.callback([{id: 1, value: 1}]);
+
+    var featureRequest = jasmine.Ajax.requests.first();
+    expect(featureRequest.url).toEqual(featureUrl);
+
+    featureRequest.response({status: '200', responseText: geojson});
+
+    var layer = getFirstLayer(map);
+    var object = layer.getLayers()[0];
+    expect(object.feature.properties.id).toEqual(1);
   });
 });
 
